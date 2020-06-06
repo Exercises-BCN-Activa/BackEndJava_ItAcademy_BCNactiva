cc16b6d92c2c32e673d7001755483a1b5544bbc1
Fauno Guazina <prof.guazina@gmail.com>
2020-06-06 23:29:04 +0200

    Update Fase03.java
    leap year verification fixes

--- Java/Ejercicio01_Variables/src/Exercici01/Fase03.java
+++ Java/Ejercicio01_Variables/src/Exercici01/Fase03.java
@@ -6,18 +6,16 @@ public class Fase03 extends Fase02 {
 	protected boolean traspas_verify;
 	
 	
-	
 	public Fase03(String nom, String cognom1, String cognom2, int dia, int mes, int any) {
 		super(nom, cognom1, cognom2, dia, mes, any);
-		// TODO Auto-generated constructor stub
 	}
 	
 	protected boolean isTraspas_verify() {
 		return traspas_verify;
 	}
-
-	protected void verifyAny() {
-		if (this.getAny() % 4 == 0 && this.getAny() % 100 != 0 && this.getAny() % 400 == 0) {
+	
+	public void verifyAny() {
+		if (this.getAny() % 4 == 0 && this.getAny() % 100 != 0 || this.getAny() % 400 == 0) {
 			traspas_verify = true;
 		} else {
 			traspas_verify = false;
@@ -25,9 +23,10 @@ public class Fase03 extends Fase02 {
 	}
 	
 	protected void printVerify() {
+		verifyAny();
 		System.out.println("\n");
 		System.out.print("L’any de naixement és de traspàs??? ");
-		if (traspas_verify) {
+		if (isTraspas_verify()) {
 			System.out.println("Sí, segur!");
 		} else {
 			System.out.println("no, definitivament!");
@@ -37,9 +36,9 @@ public class Fase03 extends Fase02 {
 	@Override
 	public void printFase03() {
 		System.out.println("-------- FASE 03 --------");
-		System.out.println("Els anys de traspàs des de " + ANY_TRASPAS +
+		System.out.println("Els anys de traspàs des de " + this.getANY_TRASPAS() +
 							" fins arriba el " + this.getAny() + ":");
-		for (int c = 1; c <= comptador_anys; c++) {
+		for (int c = 1; c <= this.getComptador_anys(); c++) {
 			System.out.print((this.getANY_TRASPAS() + (c*4)) + " | ");
 		}
 		printVerify();
